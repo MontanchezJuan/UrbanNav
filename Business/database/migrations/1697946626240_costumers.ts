c4c4f0698d7780cf0e57c3cc307592d55f80181a
MontanchezJuan <juanfelipemontanchez@gmail.com>
2023-10-22 20:39:50 -0500

    migration updated

--- Business/database/migrations/1697946626240_costumers.ts
+++ Business/database/migrations/1697946626240_costumers.ts
@@ -6,10 +6,8 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-
-      /**
-       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
-       */
+      table.string('user_id').notNullable
+      table.integer('status')
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
     })
