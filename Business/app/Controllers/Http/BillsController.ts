1c497e1ea55e4eced2694d88e1e16b8f8b410ad8
MontanchezJuan <juanfelipemontanchez@gmail.com>
2023-10-25 08:39:04 -0500

    bill model created

--- Business/app/Controllers/Http/BillsController.ts
+++ Business/app/Controllers/Http/BillsController.ts
@@ -1,35 +1,36 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Customer from 'App/Models/Customer'
+import Bill from 'App/Models/Bill'
 
-export default class CostumersController {
+export default class BillsController {
   public async store({ request }: HttpContextContract) {
     let body = request.body()
-    const customer = await Customer.create(body)
-    return customer
+    const bill = await Bill.create(body)
+    return bill
   }
 
   public async index({ request }: HttpContextContract) {
     const page = request.input('page', 1)
     const perPage = request.input('per_page', 20)
-    let customers: Customer[] = await Customer.query().paginate(page, perPage)
-    return customers
+    let bills: Bill[] = await Bill.query().paginate(page, perPage)
+    return bills
   }
 
   public async show({ params }: HttpContextContract) {
-    return Customer.findOrFail(params.id)
+    return Bill.findOrFail(params.id)
   }
 
   public async update({ params, request }: HttpContextContract) {
     const body = request.body()
-    const customer: Customer = await Customer.findOrFail(params.id)
-    customer.user_id = body.user_id
-    customer.status = body.status
-    return customer.save()
+    const bill: Bill = await Bill.findOrFail(params.id)
+    bill.service_id = body.service_id
+    bill.credit_card_id = body.credit_card_id
+    bill.status = body.status
+    return bill.save()
   }
 
   public async destroy({ params, response }: HttpContextContract) {
-    const customer: Customer = await Customer.findOrFail(params.id)
+    const bill: Bill = await Bill.findOrFail(params.id)
     response.status(204)
-    return customer.delete()
+    return bill.delete()
   }
 }
