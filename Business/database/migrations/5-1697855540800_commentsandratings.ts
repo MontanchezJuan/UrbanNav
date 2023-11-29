a46093a9bb3049d4c2c81c73eb0371c7bbe93578
nocilascg <nicolas99cg@gmail.com>
2023-11-29 14:44:20 -0500

    CommentsAndRatings Changes

--- Business/database/migrations/5-1697855540800_commentsandratings.ts
+++ Business/database/migrations/5-1697855540800_commentsandratings.ts
@@ -6,11 +6,11 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.integer('sender_id').notNullable()
-      table.integer('receptor_id').notNullable()
+      table.integer('service_id')
+      table.boolean('byCustomer').notNullable()
       table.string('description')
       table.integer('rating')
-      table.integer('status').notNullable()
+      table.integer('status').notNullable().defaultTo(0)
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
     })
